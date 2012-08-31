5c69787ed3b244d243acf6541108f56255122a9b
gabi <gabi@gabi-VirtualBox>
2012-08-31 15:32:43 +0200

    define vars

--- Heizung2/src/com/github/heizung2/MainActivity.java
+++ Heizung2/src/com/github/heizung2/MainActivity.java
@@ -5,13 +5,35 @@ import java.net.URL;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
 
+
  
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+	protected Button Button1;
+	protected Button Button2;
+	protected Button Button3;
+	protected Button Button4;
+	protected Button Button5;
+	protected Button Button6;
+	protected Button Button7;
+
+	protected TextView SelectedLabel;
+
+
+	protected int[] nrofpng;
+	protected int sel1;
+	protected int sel2;
+	protected String grafurl;
+	
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +44,15 @@ public class MainActivity extends Activity {
         Drawable drawable = LoadImageFromWeb("http://www.lcc-wien.at/images/stories/lichtscheidl/mastersms_234a.jpg");
         imgView.setImageDrawable(drawable);
  		*/
- 
+        sel1 = 1;
+        sel2 = 1;
+        
+        
+        grafurl="http://keyrate2.dyndns.org:8181/rrd/png"+sel1+".png";
+        
         ImageView imgView =(ImageView)findViewById(R.id.ImageView1);
         
-        UrlImageViewHelper.setUrlDrawable(imgView, "http://keyrate2.dyndns.org:8181/rrd/png2.png");
+        UrlImageViewHelper.setUrlDrawable(imgView, grafurl);
         
         
     }
